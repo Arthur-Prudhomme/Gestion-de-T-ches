7f67cb551d9997bd0f1e6b327471cd6db3c3f934
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-02-28 14:10:55 +0100

    dynamic task list

--- app.js
+++ app.js
@@ -10,10 +10,55 @@ function addTask(title, description, date, priority, category) {
 function editTask(id, title, description, date, priority, category) {
     TaskClass.editTask(id, title, description, date, priority, category);
 }
-var addTaskButton = document.querySelector("#addTaskButton");
+//################# add task #################
 var form = document.forms["addTaskForm"];
 form.addEventListener("submit", () => {
     addTask(form.title.value, form.description.value, form.date.value, form.priority.value);
+    form.reset();
+    addTasksDOM();
 });
+//############################################
+//################# edit task #################
 var editTaskButton = document.querySelector("#editTaskButton");
 editTaskButton.addEventListener("click", () => editTask(3, "testEdit", "testDescEdit", new Date(), "high"));
+//#############################################
+//################# add task in DOM #################
+var taskContainer = document.querySelector("#tasks-container");
+function addTasksDOM() {
+    taskContainer.innerHTML = "";
+    TaskClass.tasks.forEach((task) => {
+        addTaskDOM(task);
+    });
+}
+addTasksDOM();
+function addTaskDOM(task) {
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task", task.priority);
+    const taskTitle = document.createElement("h3");
+    taskTitle.textContent = task.title;
+    const span = document.createElement("span");
+    if (task.priority == "high")
+        span.textContent = "– Priorité Haute";
+    if (task.priority == "medium")
+        span.textContent = "– Priorité Moyenne";
+    if (task.priority == "low")
+        span.textContent = "– Priorité Faible";
+    taskTitle.appendChild(span);
+    const deadlineParagraph = document.createElement("p");
+    deadlineParagraph.textContent = `Date d'échéance: ${task.date}`;
+    const descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = task.description;
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.textContent = "Supprimer";
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-btn");
+    editButton.textContent = "Modifier";
+    taskDiv.appendChild(taskTitle);
+    taskDiv.appendChild(deadlineParagraph);
+    taskDiv.appendChild(descriptionParagraph);
+    taskDiv.appendChild(deleteButton);
+    taskDiv.appendChild(editButton);
+    taskContainer.appendChild(taskDiv);
+}
+//###################################################
--- app.ts
+++ app.ts
@@ -39,7 +39,7 @@ function editTask(
 	TaskClass.editTask(id, title, description, date, priority, category);
 }
 
-var addTaskButton = document.querySelector("#addTaskButton");
+//################# add task #################
 var form = document.forms["addTaskForm"];
 form.addEventListener("submit", () => {
 	addTask(
@@ -48,9 +48,61 @@ form.addEventListener("submit", () => {
 		form.date.value,
 		form.priority.value
 	);
+	form.reset();
+	addTasksDOM();
 });
+//############################################
 
+//################# edit task #################
 var editTaskButton = document.querySelector("#editTaskButton");
 editTaskButton.addEventListener("click", () =>
 	editTask(3, "testEdit", "testDescEdit", new Date(), "high")
 );
+//#############################################
+
+//################# add task in DOM #################
+var taskContainer = document.querySelector("#tasks-container");
+function addTasksDOM() {
+	taskContainer.innerHTML = "";
+	TaskClass.tasks.forEach((task) => {
+		addTaskDOM(task);
+	});
+}
+addTasksDOM();
+
+function addTaskDOM(task: TaskClass) {
+	const taskDiv: HTMLDivElement = document.createElement("div");
+	taskDiv.classList.add("task", task.priority);
+
+	const taskTitle: HTMLHeadingElement = document.createElement("h3");
+	taskTitle.textContent = task.title;
+	const span: HTMLSpanElement = document.createElement("span");
+	if (task.priority == "high") span.textContent = "– Priorité Haute";
+	if (task.priority == "medium") span.textContent = "– Priorité Moyenne";
+	if (task.priority == "low") span.textContent = "– Priorité Faible";
+	taskTitle.appendChild(span);
+
+	const deadlineParagraph: HTMLParagraphElement = document.createElement("p");
+	deadlineParagraph.textContent = `Date d'échéance: ${task.date}`;
+
+	const descriptionParagraph: HTMLParagraphElement =
+		document.createElement("p");
+	descriptionParagraph.textContent = task.description;
+
+	const deleteButton: HTMLButtonElement = document.createElement("button");
+	deleteButton.setAttribute("type", "button");
+	deleteButton.textContent = "Supprimer";
+
+	const editButton: HTMLButtonElement = document.createElement("button");
+	editButton.classList.add("edit-btn");
+	editButton.textContent = "Modifier";
+
+	taskDiv.appendChild(taskTitle);
+	taskDiv.appendChild(deadlineParagraph);
+	taskDiv.appendChild(descriptionParagraph);
+	taskDiv.appendChild(deleteButton);
+	taskDiv.appendChild(editButton);
+
+	taskContainer.appendChild(taskDiv);
+}
+//###################################################
