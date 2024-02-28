2dc0d891e633b647bbf77cacbcddc89b0052d2fe
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-02-28 16:05:54 +0100

    updated edit with form

--- app.ts
+++ app.ts
@@ -48,24 +48,65 @@ function removeTask(id: number) {
 //###############################################
 
 //################# add task #################
-var form = document.forms["addTaskForm"];
-form.addEventListener("submit", () => {
+var addTaskForm = document.forms["addTaskForm"];
+addTaskForm.addEventListener("submit", () => {
 	addTask(
-		form.title.value,
-		form.description.value,
-		form.date.value,
-		form.priority.value
+		addTaskForm.title.value,
+		addTaskForm.description.value,
+		addTaskForm.date.value,
+		addTaskForm.priority.value
 	);
-	form.reset();
+	addTaskForm.reset();
 	addTasksDOM();
 });
 //############################################
 
 //################# edit task #################
-var editTaskButton = document.querySelector("#editTaskButton");
-editTaskButton.addEventListener("click", () =>
-	editTask(3, "testEdit", "testDescEdit", new Date(), "high")
-);
+function editTaskForm(id: number) {
+	let editTaskForm = document.forms["editTaskForm"];
+	editTaskForm.addEventListener("submit", () => {
+		editTask(
+			id,
+			editTaskForm.title.value,
+			editTaskForm.description.value,
+			editTaskForm.date.value,
+			editTaskForm.priority.value
+		);
+		addTasksDOM();
+	});
+}
+
+function editMode(task: TaskClass) {
+	addTasksDOM();
+	let taskDiv = document.getElementById(`task-n-${task.id}`);
+	taskDiv.innerHTML = `
+	<form name="editTaskForm" id="taskForm" onsubmit="return false">
+	<input type="text" id="editTaskTitle" value="${task.title}" name="title"/>
+	<input type="date" id="editTaskDate" value="${task.date}" name="date"/>
+	<textarea type="text" id="editTaskDesc" name="description">${
+		task.description
+	}</textarea>
+
+	<select id="editTaskPriority" name="priority">
+	<option value="low" ${task.priority == "low" ? "selected" : ""}>Faible</option>
+	<option value="medium" ${
+		task.priority == "medium" ? "selected" : ""
+	}>Moyenne</option>
+	<option value="high" ${task.priority == "high" ? "selected" : ""}>Haute</option>
+	</select>
+	
+	<button type="button" id="cancel-button">Annuler</button>
+	<button class="edit-btn" id="apply-edit-button">Appliquer</button>
+	</form>`;
+
+	taskDiv
+		.querySelector("#cancel-button")
+		.addEventListener("click", () => addTasksDOM());
+	taskDiv
+		.querySelector("#apply-edit-button")
+		.addEventListener("click", () => editTaskForm(task.id));
+	console.log("editMode");
+}
 //#############################################
 
 //################# add task in DOM #################
@@ -89,11 +130,14 @@ function addTaskDOM(task: TaskClass) {
 	<p>Date d'échéance: ${task.date}</p>
 	<p>${task.description}</p>
 	<button type="button" id="delete-button">Supprimer</button>
-	<button class="edit-btn">Modifier</button>
-	`;
+	<button class="edit-btn" id="edit-button">Modifier</button>`;
+
 	taskDiv
 		.querySelector("#delete-button")
 		.addEventListener("click", () => removeTask(task.id));
+	taskDiv
+		.querySelector("#edit-button")
+		.addEventListener("click", () => editMode(task));
 
 	taskContainer.appendChild(taskDiv);
 }
